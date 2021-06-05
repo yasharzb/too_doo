2e8ff3df6c33cb2164f2c6136103d473fdb15172
yasharzb <yasharzb@ce.sharif.edu>
2021-06-05 10:37:46 -0400

    Add handle_cmd function

--- too_doo/main.swift
+++ too_doo/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+func handle_cmd(inp: String) -> Bool{
+    let inpArray = inp.components(separatedBy: " ")
+    let cmd: String = inpArray[ParamOrder.CMD.rawValue].uppercased()
+    let command = Command(rawValue: cmd)
+    switch command {
+    case .HELP:
+        print("Help")
+    case .ADD:
+        print("Add")
+    case .CREATE:
+        print("Create")
+    case .VIEW:
+        print("View")
+    case .VIEW_ALL:
+        print("View all")
+    case .EDIT:
+        print("Edit")
+    case .DELETE:
+        print("Delete")
+    case .EXIT:
+        return false
+    default:
+        print("Default")
+    }
+    return true
+}
 
+print("Enter a command. help for commands' help")
+
+var isRunning = true
+while isRunning {
+    let inp = readLine()
+    isRunning = handle_cmd(inp: inp!)
+}
